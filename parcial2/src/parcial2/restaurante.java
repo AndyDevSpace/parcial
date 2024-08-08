cb17caccb81629be00d590dec09bab445439d86d
Andres Galindo <165195709+AndyDevSpace@users.noreply.github.com>
2024-08-08 10:54:06 -0500

    Se subieron los archivos sin bash.
    feat: Commit inicial parcial
    
    Se creo un modelo se tabla restaurante donde se puede subir los platos.
    - Se implemento únicamente la funcionalidad de agregar plato.
    - Se creo una base de datos usando mysql.

---
+++ parcial2/src/parcial2/restaurante.java
@@ -0,0 +1,13 @@
+
+package parcial2;
+
+public class restaurante {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
